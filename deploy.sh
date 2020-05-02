c49908fa8d805787ec5eda874e73d3ba7447bacd
向仕林 <295755973@qq.com>
2020-05-02 19:45:47 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cname_file=$(pwd)/public/CNAME
+build_dir=$(pwd)/dist
+origin_address=git@github.com:ResonateWithMe/blog-admin-web.git
+
+function build() {
+    echo "start build..."
+    yarn install && yarn build
+}
+
+function pushBuild() {
+    echo push "$build_dir" to master barch
+    cp "$cname_file" "$build_dir"
+    cd "$build_dir"
+    git init && git add -A && git commit -m 'chore: update' && git push -f $origin_address master
+}
+
+build
+pushBuild
